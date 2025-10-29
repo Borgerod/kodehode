2b6fa6dafcb20bb640b9c0c0030b0ffd5288be24
Borgerod <borgerod@hotmail.com>
2025-10-29 15:09:18 +0100

    Create DirCommand.cs
    Directory version of FileCommand

---
+++ Kartlegging/C#/Backend_Day2/classes/DirCommand.cs
@@ -0,0 +1,35 @@
+using System.IO; // Add this at the top
+public class DirCommand
+{
+
+    // FileInfo? info; //? dont think i need this 
+
+    public void PrintDirInformation(string name)
+    {
+        // Prints folder/dir info
+        var dirInfo = new DirectoryInfo(name);
+        FileCommand fileCommand = new FileCommand();
+        long dirSize = GetDirectorySize(name);
+        Console.WriteLine($"\n___Dir Info________________\n   Name: {dirInfo.Name}\n   Type: Directory\n   Extension: /\n   Size: {FileCommand.GetReadableFileSize(dirSize)}\n   Created: {dirInfo.CreationTime}\n   Length: {dirInfo.GetFileSystemInfos().Length}\n___________________________");
+    }
+    public void PrintDirPreview(string name)
+    {
+        WriteTo writeTo = new WriteTo();
+        writeTo.ReadContent(name);
+    }
+
+    public void EnterDir(string name)
+    {
+        // Prints folder/dir content
+        // TODO recall main with root-one stop futher in. 
+    }
+
+
+    static long GetDirectorySize(string path)
+    {
+        return Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
+                        .Sum(file => new FileInfo(file).Length);
+    }
+
+}
+
