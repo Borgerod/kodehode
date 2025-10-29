cb0c8f512c29500db0c25b81923191825eed9588
Borgerod <borgerod@hotmail.com>
2025-10-29 15:09:05 +0100

    Update fileCommand.cs

--- Kartlegging/C#/Backend_Day2/classes/fileCommand.cs
+++ Kartlegging/C#/Backend_Day2/classes/fileCommand.cs
@@ -1,37 +1,40 @@
-using System.IO; // Add this at the top
+using System.IO; // Add this at the topusing System.IO; // Add this at the top
+using Microsoft.Win32;
+
 public class FileCommand
 {
 
     // FileInfo? info; //? dont think i need this 
 
-    public void PrintFileinformation(string fileName)
+    public void PrintFileInformation(string fileName)
     {
-        //TODO function: make userinput to get the file you want
-
-        //gets metadata of file
-
         var info = new FileInfo(fileName);
-        var extension = Path.GetExtension(fileName);
-        string typeOfFile = "";
+        Console.WriteLine($"\n___Info____________________\n    name: {info.Name}\n    Type: {GetFileTypeDescription(info.Extension)}\n    Extension: {info.Extension}\n    Size: {GetReadableFileSize(info.Length)}\n    Created: {info.CreationTime}\n    Path: {info.FullName}\n___________________________");
+        return;
+    }
+    public static string GetFileTypeDescription(string extension)
+    {
+        if (string.IsNullOrEmpty(extension))
+            return "Unknown";
+        // TODO: make it MacOS and Linux compatible
+#pragma warning disable CA1416
+        using var key = Registry.ClassesRoot.OpenSubKey(extension);
+        var value = key?.GetValue("") as string;
+        if (value == null) return extension.TrimStart('.');
 
-        var metaData = info!; //TEMP
-
-         switch (extension)
-        {
-            case ".md":
-                typeOfFile = "ASCII text file";
-                break;
-            case ".cs":
-                typeOfFile = "C# source file";
-                break;
-            case ".txt":
-                typeOfFile = "Plain Text file";
-                break;
-        }
-
-        Console.WriteLine($"Filename: {fileName} \nType: {typeOfFile} \nExtension: {metaData.Extension}"); //TEMP
+        using var typeKey = Registry.ClassesRoot.OpenSubKey(value);
+        return typeKey?.GetValue("") as string ?? extension.TrimStart('.');
     }
 
+    // changed: make this public so other classes (DirCommand) can call it
+    public static string GetReadableFileSize(long bytes)
+    {
+        string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+        if (bytes == 0) return "0 B";
+        int order = (int)Math.Floor(Math.Log(bytes, 1024));
+        double adjustedSize = bytes / Math.Pow(1024, order);
+        return $"{adjustedSize:0.##} {sizes[order]}";
+    }
 }
 
 
