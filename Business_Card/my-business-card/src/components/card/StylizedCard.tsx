ed46c835ac7524a610d9fcb51d3a22e2725e08ca
Borgerod <borgerod@hotmail.com>
2025-11-10 12:42:36 +0100

    Update StylizedCard.tsx
    applied grid and stacking.
    
    todo (maybe) text might be clipped, might need to take care of that later.

--- Business_Card/my-business-card/src/components/card/StylizedCard.tsx
+++ Business_Card/my-business-card/src/components/card/StylizedCard.tsx
@@ -1,18 +1,17 @@
-//> Placeholder
+// //> Placeholder
 "use client";
-
 import { Card } from "@/components/card/Card";
 import FrostedGlass from "@/components/card/FrostedGlass";
 import { Button } from "@/components/button/Button";
 import { Tag } from "@/components/button/Tag";
 import { LuChevronsRight } from "react-icons/lu";
 import { JSX } from "react";
-import { StylizedCircle } from "../accessories/stylized_circle";
+import { StylizedCircle } from "../accessories/StylizedCircle";
+import { cn } from "@/lib/utils";
 
 function getContent(): JSX.Element {
 	return (
 		<>
-			{/* Card content */}
 			<h3 className="text-xm">MY NUMBERS</h3>
 
 			<div className="flex flex-row justify-start gap-5">
@@ -52,16 +51,30 @@ function getContent(): JSX.Element {
 	);
 }
 
-export default function WorkExperience() {
+export default function StylizedCard() {
 	return (
-		<Card className="bg-[var(--color-surface-green-primary)] p-0 m-0 text-text-white-translucent-primary font-light">
-			{/*  */}
-			{/* <div className="w-full h-40 flex items-center justify-end p-2"></div> */}
-			{/* TODO: create mask */}
-			<div className="w-full h-40 flex items-center justify-end p-2">
-				<StylizedCircle className="rounded-full h-full aspect-square translate-x-1/2" />
+		<Card
+			disableInnerWrapper
+			className={cn(
+				"relative rounded-2xl   text-text-white-translucent-primary font-light p-0 m-0 w-full h-full min-h-0",
+				"bg-transparent",
+				"grid [grid-template-areas:'stack'] grid-rows-[1fr] grid-cols-[1fr] place-items-center",
+				"overflow-clip"
+			)}
+		>
+			<div className="[grid-area:stack] relative h-full w-full m-0 overflow-visible bg-transparent pt-4">
+				<StylizedCircle className="h-[80%] aspect-square rounded-full bg-gradient-to-br place-self-end translate-x-3/6 filter blur-xs opacity-90" />
 			</div>
-			{/* {getContent()} */}
+
+			<div
+				className={cn(
+					"[grid-area:stack] relative z-10 w-full h-full p-0 m-0",
+					"bg-transparent",
+					"text-secondary"
+				)}
+			>
+				{getContent()}
+			</div>
 		</Card>
 	);
 }
